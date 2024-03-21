6e3c3e4efe94d5ee114768494e5685615756f03c
PolinaAndreyanova <andreyanovalina@yandex.ru>
2024-03-21 15:54:45 +0000

    добавить css-стили

--- form-handler.php
+++ form-handler.php
@@ -19,17 +19,14 @@ SaveHistory([$arData[3], $arData[1], $arData[2], $tagCount]);
 <html lang="ru">
 <head>
     <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./style.css" />
     <title>Результаты поиска</title>
-    <style>
-        input {
-            margin-top: 20px;
-        }
-    </style>
 </head>
-<body>
-    <p><?=$feedback?></p>
-    <form action="index.php" method="post">
-        <input type="submit" value="На главную!" />
+<body class="content">
+    <form class="content__form" action="index.php" method="post">
+        <p class="content__feedback"><?=$feedback?></p>
+        <button class="content__button" type="submit">На главную</button>
     </form>
 </body>
 </html>
--- index.php
+++ index.php
@@ -2,19 +2,17 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./style.css" />
     <title>Форма поиска</title>
-    <style>
-        input {
-            margin-top: 20px;
-        }
-    </style>
 </head>
-<body>
-    <form action="form-handler.php" method="post">
-        URL: <input type="url" name="url" value="" required /><br>
-        tag: <input type="text" name="tag" value="" required /><br>
-        class: <input type="text" name="class" value="" /><br>
-        <input type="submit" value="Найти!" />
+<body class="content">
+    <form class="content__form" action="form-handler.php" method="post">
+        <h1 class="content__header">Форма поиска</h1>
+        <input class="content__input" type="url" name="url" value="" placeholder="url" required /><br>
+        <input class="content__input" type="text" name="tag" value="" placeholder="tag" required /><br>
+        <input class="content__input" type="text" name="class" value="" placeholder="class" /><br>
+        <button class="content__button" type="submit">Найти</button>
     </form>
 </body>
 </html>
